6da8262d6b7a8d5dd69872f12ab9443bd7bba873
biancafaur <faur.biancaloredana@yahoo.com>
2014-06-11 09:52:26 +0300

    to do app reviewed

--- functions.js
+++ functions.js
@@ -1,187 +1,202 @@
-function showNotes() {
-	var toDoNotes = getToDoNotesArray();
-	for (var i = 0; i < toDoNotes.length; i++) {
-		var key = toDoNotes[i];
-		var value = JSON.parse(localStorage[key]);
-		displayAllNotes(key,value);
-		}
-}
+var stickyNote = (function() {
 
-function getToDoNotesArray() {
-	var toDoNotes = localStorage.getItem("toDoNotes");
-	if(!toDoNotes) {
-		toDoNotes = [];
-		localStorage.setItem("toDoNotes",JSON.stringify(toDoNotes));
-		} else {
-			toDoNotes = JSON.parse(toDoNotes);
-		}	
-		return toDoNotes;
-}
-	
-function displayAllNotes(key,notesObj) {
-	var tasks = document.getElementById("tasks");
-	var div = document.createElement("div");
-	div.setAttribute("class","task");
-	var pin = document.createElement("div");
-	pin.setAttribute("class","pin");
-	var status = document.createElement("div");
-	var checkbox = document.createElement('input');
-	checkbox.type = "checkbox";
-	checkbox.setAttribute("class","checkStatus");
-	var label = document.createElement("label");
-	if(notesObj.status == "to-do") {
-		label.innerHTML = "To do";
-		status.appendChild(label);
-		status.appendChild(checkbox);
-	} else {
-		checkbox.setAttribute("checked",true);
-		label.innerHTML = "Done";
-		status.appendChild(label);
-		status.appendChild(checkbox);
-	}
-	status.setAttribute("class","status");
-	div.innerHTML = notesObj.value;
-	div.appendChild(pin);
-	div.appendChild(status);
-	div.setAttribute("id",key);
-	tasks.appendChild(div);
-}
-
-function addNewNote() {
-	var value = document.getElementById("note").value;
-	
-	var toDoNotes = getToDoNotesArray();
-	var length = toDoNotes.length;
-	var key = "note_" + length;
-
-	var notesObj = {
-		"value" : value,
-		"status" : "to-do"
-	}
-	if(value !== "") {
-		localStorage.setItem(key, JSON.stringify(notesObj));
-		toDoNotes.push(key);
-		localStorage.setItem("toDoNotes", JSON.stringify(toDoNotes));
-		displayAllNotes(key,notesObj);
-		value.value = "";
-		}
-}
-
-function selectedCateg() {
-	$(".category").click(function() {
-		$(".category").removeClass("selected");
-		$(this).addClass("selected");
-	});
-}
-
-function deleteNote(key) {
-	var toDoNotes = getToDoNotesArray();
-	
-	$("div").on("click","div.pin", function() {
-		var key = $(this).parent().attr("id");
-		var answer = confirm("Are you sure you want tot delete this note?");
-		if(answer) {
-			if(toDoNotes) {
-				for(var i = 0; i < toDoNotes.length; i++) {
-					if(toDoNotes[i] === key) {
-						toDoNotes.splice(i,1);
-					}
-				}
-				localStorage.setItem("toDoNotes", JSON.stringify(toDoNotes));
-			}
-			localStorage.removeItem(key);
-			removeNoteFromDOM(key);
-		}
-	});
-}
-
-function removeNoteFromDOM(key) {
-	var note = document.getElementById(key);
-	var cont = document.getElementById("tasks");
-	cont.removeChild(note);
-}
-
-function getNoteObject(key) {
-	var obj = localStorage.getItem(key);
-
-	for (var i = 0; i < localStorage.length; i++) {
-		if(localStorage.key(i) == key) {
-			obj = JSON.parse(localStorage[key]);
-		}
-	}
-	
-	return obj;
-}
-
-function toggleStatus() {
-	$("div").on("click",".checkStatus",function() {
-		var key = $(this).parent().parent().attr("id");
-		var obj = getNoteObject(key);
-		var status = obj.status;
-
-		if($(this).is(':checked')) {
-			$(this).siblings().text("Done");
-			status = "done";
-			obj = {
-				"value" : obj.value,
-				"status" : status
-				}
-			localStorage.setItem(key, JSON.stringify(obj));
-		} else {
-			$(this).siblings().text("To do");
-			status = "to-do";
-			obj = {
-				"value" : obj.value,
-				"status" : status
-				}
-			localStorage.setItem(key, JSON.stringify(obj));
-		}
-	});
-}
-
-function filterNotes() {
-	$(".category").click(function() {
-		var categ = $(this).attr("id");
-		if(categ == "all") {
-				$("#tasks").html('');
-				showNotes();
+	var toDoNotesArray = function () {
+		var notes = localStorage.getItem("notes");
+		if(!notes) {
+			notes = [];
+			localStorage.setItem("notes",JSON.stringify(notes));
 			} else {
-				$("#tasks").html('');
-				for (var i = 0; i < localStorage.length; i++) {
-						var key = localStorage.key(i);
-						var obj = JSON.parse(localStorage[key]);
-						var status = obj.status;
-						if(status == categ) {
-							displayAllNotes(key,obj);
-							}
-						}
-					
-					}
-	});
-}
+				notes = JSON.parse(notes);
+			}	
+			return notes;
+		};
+		
+	var toDoArray = function () {
+		var to_do = localStorage.getItem("to_do");
+		if(!to_do) {
+			to_do = [];
+			localStorage.setItem("to_do",JSON.stringify(to_do));
+			} else {
+				to_do = JSON.parse(to_do);
+			}	
+			return to_do;
+		};
+		
+	var doneArray = function () {
+		var done = localStorage.getItem("done");
+		if(!done) {
+			done = [];
+			localStorage.setItem("done",JSON.stringify(done));
+			} else {
+				done = JSON.parse(done);
+			}	
+			return done;
+		};
 
-function getNote() {
-	$("div").on("dblclick",".task",function() {
-		$("#edit_btn").show();
-		var text = $(this).text();
-		$("#note").val(text);
+	var notes = toDoNotesArray();
+	var to_do = toDoArray();
+	var done = doneArray();
+	
+	var renderHTML = function(categ) {
+		_.each(categ, function(i){
+			var templateString = "<div class='task' id='" + i + "'>\
+				<div class='pin'></div>\
+				<div class='status'>\
+					<input type='checkbox' class='checkStatus'>\
+					<label></label>\
+				</div>\
+				<div class='text'></div>\
+			</div>";
+			var template = _.template(templateString);
+			$("#tasks").append(template);
+		}); 
+	};
 
-		var key = $(this).attr("id");
-		$("#edit_btn").attr("name",key);
-	});
-}
-
-function editNote() {
-	$("#edit_btn").click(function() {
-		var key = $(this).attr("name");
-		var obj = getNoteObject(key);
-		var value = $("#note").val();
-		obj = {
-			"value" : value,
-			"status" : obj.status
+	var displayStickies = function(categ) {
+		renderHTML(categ);
+		for(var i = 0; i < categ.length; i++) {
+			var key = categ[i];
+			if($.inArray(key, to_do) !== -1) {
+				$("#" + key +" .status label").text("To do");
+			} else {
+				$("#" + key +" .status label").text("Done");
+				$("#" + key +" .status .checkStatus").attr("checked",true);
+				}
+			$("#" + key + " .text").text(localStorage[key]);
 		}
-		localStorage.setItem(key, JSON.stringify(obj));
-		$("#edit_btn").hide();
-		location.reload();
-	});
-}
+	};
+
+	var filterNotes = function(categ) {
+		$("#tasks").html("");
+		switch(categ) {
+			case "to_do":
+				displayStickies(to_do);
+				break;
+			case "done":
+				displayStickies(done);
+				break;
+			default: 
+				displayStickies(notes);
+		}
+	};
+
+	var saveSticky = function() {
+		var length = notes.length;
+		var key = "note_" + length;
+
+		localStorage.setItem(key, "");
+		notes.push(key);
+		to_do.push(key);
+		localStorage.setItem("notes", JSON.stringify(notes));
+		localStorage.setItem("to_do", JSON.stringify(to_do));
+	};
+
+	var editSticky = function(note) {
+		var key = $(note).parent().attr("id");
+		var value = $(note).text();
+		localStorage[key] = value;
+	};
+	
+	var toggleStatus = function() {
+		$("#tasks").on("click"," div:nth-child(2) .checkStatus",function() {
+			var key = $(this).parent().parent().attr("id");
+
+			if($(this).is(':checked')) {
+				$(this).siblings().text("Done");
+				done.push(key);
+				to_do.splice($.inArray(key, to_do),1);
+				localStorage.setItem("done",JSON.stringify(done));
+				localStorage.setItem("to_do", JSON.stringify(to_do));
+			} else {
+				$(this).siblings().text("To do");
+				to_do.push(key);
+				localStorage.setItem("to_do",JSON.stringify(to_do));
+				done.splice($.inArray(key, done),1);
+				localStorage.setItem("done", JSON.stringify(done));
+			}
+			filterNotes($(".selected").attr("id"));
+			statistics();
+		});
+	};
+
+	var checkStatus = function(key) {
+		if($.inArray(key, to_do) !== -1) {
+			to_do.splice($.inArray(key, to_do),1);
+			localStorage.setItem("to_do", JSON.stringify(to_do));
+		} else {
+			done.splice($.inArray(key, done),1);
+			localStorage.setItem("done", JSON.stringify(done));
+		}
+	};
+
+	var deleteNote = function(key) {
+		for(var i = 0; i < notes.length; i++) {
+			if(notes[i] === key) {
+				notes.splice(i,1);
+			}
+		}
+		localStorage.setItem("notes", JSON.stringify(notes));
+		localStorage.removeItem(key);
+		
+		checkStatus(key);
+		removeNoteFromDOM(key);
+		statistics();
+	};
+
+	var removeNoteFromDOM = function(id) {
+		var note = document.getElementById(id);
+		var cont = document.getElementById("tasks");
+		cont.removeChild(note);
+	};
+
+	var statistics = function() {
+		$("#notes p span").text("(" + notes.length + ")");
+		$("#to_do p span").text("(" + to_do.length + ")");
+		$("#done p span").text("(" + done.length + ")");
+	};
+
+	return {
+		toDoNotesArray : toDoNotesArray,
+		saveSticky : saveSticky,
+		editSticky : editSticky,
+		toggleStatus : toggleStatus,
+		deleteNote : deleteNote,
+		filterNotes : filterNotes,
+		statistics : statistics
+	};
+})();
+
+var general = (function() {
+	var selectCateg = function() {
+		$(".category").click(function() {
+			$(".category").removeClass("selected");
+			$(this).addClass("selected");
+			
+			var id = $(this).attr("id");
+			// $(".tasks").html("");
+			stickyNote.filterNotes(id);
+		});
+	};
+
+	var addSticky = function() {
+		$("#add_button").click(function() {
+			var templateString = "<div class='task' id='note_<%= note %>'>\
+				<div class='pin'></div>\
+				<div class='status'>\
+					<input type='checkbox' class='checkStatus'>\
+					<label>To do</label>\
+				</div>\
+				<div class='text'></div>\
+			</div>";
+			var template = _.template(templateString, {note:[stickyNote.toDoNotesArray().length]});
+			$("#tasks").append(template);
+			stickyNote.saveSticky();
+			stickyNote.statistics();
+		});
+	}; 
+
+	return {
+		selectCateg : selectCateg,
+		addSticky : addSticky
+	}
+})();
